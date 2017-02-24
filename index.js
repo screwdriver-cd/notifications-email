0b8f7c3903a5f9aaf23c76a6574b26c28556fb1e
Jerry Zhang <jerryzhang@yahoo-inc.com>
2017-02-24 10:49:34 -0800

    Fix rough work on EmailNotifier

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const SCHEMA_ADDRESS = Joi.string().email();
 const SCHEMA_ADDRESSES = Joi.array()
     .items(SCHEMA_ADDRESS)
     .min(0);
-const SCHEMA_STATUSES = Joi.array()
-    .min(0);
-const SCHEMA_EMAIL = Joi.object()
+const SCHEMA_STATUS = Joi.string();
+const SCHEMA_BUILD_DATA = Joi.object()
     .keys({
         addresses: SCHEMA_ADDRESSES,
-        statuses: SCHEMA_STATUSES
+        status: SCHEMA_STATUS
+    });
+
+const SCHEMA_SMTP_CONFIG = Joi.object()
+    .keys({
+        host: Joi.string(),
+        port: Joi.number().integer()
     });
 
 const DESCRIPTION_MAP = {
@@ -24,13 +29,13 @@ const DESCRIPTION_MAP = {
 
 class EmailNotifier {
     notify(buildData) {
+        Joi.attempt(buildData, SCHEMA_BUILD_DATA, 'Invalid build data format');
         const message = DESCRIPTION_MAP[buildData.status];
     }
 
     constructor(config = {}, server) {
-        this.config = Joi.attempt(config, Joi.object().keys({
-            email: SCHEMA_EMAIL
-        }).unknown(true), 'Invalid config for Notifications (Email)');
+        this.config = Joi.attempt(config, SCHEMA_SMTP_CONFIG.unknown(true),
+            'Invalid config for Notifications (Email)');
         this.server = server;
         server.on('notify', this.notify);
     }
